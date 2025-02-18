3122201e76c87f6ce304758701b6bb6031b1f818
hello-vik <vikash562001k@gmail.com>
2025-02-18 20:31:34 +0530

    jgjk

--- app/src/main/java/com/example/studentattendance/MainActivity.kt
+++ app/src/main/java/com/example/studentattendance/MainActivity.kt
@@ -7,8 +7,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-//hello
-//world
+//jgjk
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
